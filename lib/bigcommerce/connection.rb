96a4ea969e4d2ee54ccbddf0085cabedc7bf31fe
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-08-16 00:34:25 +0200

    Removeing puts for debugging

--- lib/bigcommerce/connection.rb
+++ lib/bigcommerce/connection.rb
@@ -82,7 +82,6 @@ module BigCommerce
       request['If-Modified-Since'] = modified_since if modified_since
       response = [:post, :put].include?(method) ? http.request(request, params) : http.request(request) 
       
-      p response, response.body, '----------------------'
       return case response
                when Net::HTTPSuccess, Net::HTTPRedirection
                  JSON.parse(response.body || "{}")
