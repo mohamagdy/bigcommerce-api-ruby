b3edd796624db1f02e4b8c24d9a31115f4d95b06
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-08-15 16:53:48 +0200

    Testing update product

--- lib/bigcommerce/connection.rb
+++ lib/bigcommerce/connection.rb
@@ -80,7 +80,7 @@ module BigCommerce
       request.add_field 'Accept', 'application/json'
       request.add_field 'Content-Type', 'application/json'
       request['If-Modified-Since'] = modified_since if modified_since
-      response = [:post, :put].include?(method) ? http.request_put(request, params) : http.request(request) 
+      response = [:post, :put].include?(method) ? http.request(request, params) : http.request(request) 
 
       return case response
                when Net::HTTPSuccess, Net::HTTPRedirection
